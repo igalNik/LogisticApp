bce3591faf7273b4b2b80fefe8c9b7f205de15aa
igal <igal.nikolaev@gmail.com>
2025-02-12 14:13:24 +0200

    Add: install dotenv packege for environment variable management

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config({ path: './config.env' });
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).send('success!'));
+app.get('/api/v1/users', (req, res) => res.status(200).json(process.env));
 
-const port = 8000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  if (process.env.NODE_ENV === 'develoopment')
+    console.log(`App running on port ${port}`);
 });
