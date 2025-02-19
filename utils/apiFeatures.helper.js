cf5456c91ae4617b1eead15cf975b3045f9043e8
igal <igal.nikolaev@gmail.com>
2025-02-19 12:55:25 +0200

    Add: APIFeatures helper
    Add an API features helper that is responsible for complex filtering, field selection, limiting, and sorting functionality.

---
+++ utils/apiFeatures.helper.js
@@ -0,0 +1,51 @@
+class APIFeatures {
+  /**
+   *
+   */
+  constructor(modelQuery, queryStringObj) {
+    this.modelQuery = modelQuery;
+    this.queryStringObj = queryStringObj;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryStringObj };
+    const excludedFields = ['sort', 'fields', 'page', 'limit'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lte|lt|inc)\b/g,
+      (match) => `$${match === 'inc' ? 'regex' : match}`
+    );
+
+    this.modelQuery = this.modelQuery.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryStringObj.sort) {
+      const sortBy = this.queryStringObj.sort.replace(/,/g, ' ');
+      this.modelQuery.sort(sortBy);
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryStringObj.fields) {
+      const fields = this.queryStringObj.fields.replace(/,/g, ' ');
+      this.modelQuery.select(fields);
+    }
+    return this;
+  }
+
+  paginate() {
+    let page = Number(this.queryStringObj.page) || 1;
+    let limit = Number(this.queryStringObj.limit) || 100;
+    const skip = (page - 1) * limit;
+
+    this.modelQuery.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
